8e0f7af020dc7d8be5f705811750e374f0bc8115
Miriamvdb <miriamvdb@live.nl>
2022-08-01 20:09:28 +0200

    Feature 5: Display articles for each category

--- src/pages/CategoryPage/index.js
+++ src/pages/CategoryPage/index.js
@@ -1,22 +1,79 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { NavLink, useParams } from "react-router-dom";
 import { FiChevronsRight } from "react-icons/fi";
-import { NavLink } from "react-router-dom";
 import "./styles.css";
 
 const CategoryPage = () => {
+  const [categories, setCategories] = useState([]);
+  const [categoryArticles, setCategoryArticles] = useState([]);
+  const params = useParams();
+
+  useEffect(() => {
+    const getCategory = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:4000/categories/${params.id}/articles`
+        );
+        console.log(response.data.articles);
+        setCategories(response.data);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    getCategory();
+
+    const getCategoryArt = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:4000/categories/${params.id}/articles`
+        );
+        console.log(response.data.articles);
+        setCategoryArticles(response.data.articles);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    getCategoryArt();
+  }, [params.id]);
+
   return (
     <div className="container-categorypage">
       <div className="title-categorypage">
-        <h1>Categories</h1>
+        <h1>
+          Articles{" "}
+          <FiChevronsRight
+            style={{ color: "tomato" }}
+            className="icon-categorypage"
+          />
+          {categories.name}
+        </h1>
       </div>
-      {/* <div className="button-icon">
-        <NavLink to="/">
-          <button className="button-categorypage">
-            <h3 className="text-button">
-              Go to articles <FiChevronsRight className="icon-categorypage" />
-            </h3>
-          </button>
-        </NavLink>
-      </div> */}
+      <div className="container-category-article">
+        {categoryArticles.map((article, index) => {
+          return (
+            <div className="category-article" key={index}>
+              <h2>{article.title}</h2>
+              <img
+                className="image-category-article"
+                src={article.imageUrl}
+                alt=""
+              />
+              <p>
+                <b>By</b> {article.author}
+              </p>
+              <NavLink
+                style={{ color: "tomato" }}
+                to={`/articledetails/${article.id}`}
+              >
+                <h2>
+                  <FiChevronsRight />
+                </h2>
+              </NavLink>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
